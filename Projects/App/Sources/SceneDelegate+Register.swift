a60be1e29e1a4aaffbe0a48db136732a7c41bc90
kangho lee <ibcylon@naver.com>
2023-12-15 23:10:21 +0900

    Mod: DI Container add Episode and Location Feature

--- Projects/App/Sources/SceneDelegate+Register.swift
+++ Projects/App/Sources/SceneDelegate+Register.swift
@@ -16,6 +16,8 @@ import CharacterInterface
 import Character
 import Location
 import LocationInterface
+import EpisodeInterface
+import Episode
 
 extension AppDelegate {
   var container: DIContainer {
@@ -28,7 +30,28 @@ extension AppDelegate {
       interface: FetchCharacterUseCaseInterface.self,
       implement: {
         FetchRMCharacterUseCase(
-          repository: CharacterRepository(characterService: DefaultCharacterService(endPoint: APIComponent(endPoint: .character)))
+          repository: CharacterRepository(
+            characterService: DefaultCharacterService(endPoint: APIComponent(endPoint: .character)))
+        )
+      }
+    )
+
+    container.register(
+      interface: FetchLocationUseCaseInterface.self,
+      implement: {
+        FetchRMLocationUseCase(
+          repository: LocationRepository(
+            locationService: DefaultLocationService(endPoint: APIComponent(endPoint: .location)))
+        )
+      }
+    )
+
+    container.register(
+      interface: FetchEpisodeUseCaseInterface.self,
+      implement: {
+        FetchRMEpisodeUseCase(
+          repository: EpisodeRepository(
+            episodeService: DefaultEpisodeService(endPoint: APIComponent(endPoint: .episode)))
         )
       }
     )
